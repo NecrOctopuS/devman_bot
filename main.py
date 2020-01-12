0580b5ac03fa28fc3f5a0a494b1cbb0ffd6a89bf
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-01-12 16:54:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,29 +16,30 @@ url = 'https://dvmn.org/api/long_polling/'
 headers = {
     'Authorization': 'Token ' + DEVMAN_TOKEN,
 }
-response = requests.get(url, headers=headers)
+params = {}
 while True:
-    if response.json()['status'] == 'timeout':
-        params = {
-            'timestamp': response.json()['timestamp_to_request']
-        }
-    elif response.json()['status'] == 'found':
-        attempts = response.json()['new_attempts']
-        for attempt in attempts:
-            bot.send_message(chat_id=TELEGRAM_ID, text=f"У вас проверили работу {attempt['lesson_title']} \n"
-                                                       f" {'https://dvmn.org' + attempt['lesson_url']}")
-            if attempt['is_negative']:
-                bot.send_message(chat_id=TELEGRAM_ID, text=f"В работе нашлись ошибки")
-            else:
-                bot.send_message(chat_id=TELEGRAM_ID, text=f"В работе нет ошибок")
-        params = {
-            'timestamp': response.json()['new_attempts'][0]['timestamp']
-        }
     try:
         response = requests.get(url, headers=headers, params=params)
+        if response.json()['status'] == 'timeout':
+            params = {
+                'timestamp': response.json()['timestamp_to_request']
+            }
+        elif response.json()['status'] == 'found':
+            attempts = response.json()['new_attempts']
+            for attempt in attempts:
+                bot.send_message(chat_id=TELEGRAM_ID, text=f"У вас проверили работу {attempt['lesson_title']} \n"
+                                                           f" {'https://dvmn.org' + attempt['lesson_url']}")
+                if attempt['is_negative']:
+                    bot.send_message(chat_id=TELEGRAM_ID, text=f"В работе нашлись ошибки")
+                else:
+                    bot.send_message(chat_id=TELEGRAM_ID, text=f"В работе нет ошибок")
+            params = {
+                'timestamp': response.json()['new_attempts'][0]['timestamp']
+            }
     except requests.exceptions.ReadTimeout:
         time.sleep(5)
     except requests.exceptions.ConnectionError:
         time.sleep(5)
     except ConnectionResetError:
         time.sleep(5)
+
