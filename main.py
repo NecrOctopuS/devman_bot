b2e13ee29b7c3fb559e05e61e5aab79fae3f3af1
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-01-27 20:59:54 +0300

    Add logging in telegram

--- main.py
+++ main.py
@@ -5,43 +5,62 @@ from dotenv import load_dotenv
 import os
 import logging
 
-logging.warning('Бот запущен')
-load_dotenv()
 DEVMAN_TOKEN = os.environ['DEVMAN_TOKEN']
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_ID = int(os.environ['TELEGRAM_ID'])
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
-url = 'https://dvmn.org/api/long_polling/'
-headers = {
-    'Authorization': 'Token ' + DEVMAN_TOKEN,
-}
-params = {}
-while True:
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        json_data = response.json()
-        if json_data['status'] == 'found':
-            attempts = json_data['new_attempts']
-            for attempt in attempts:
-                if attempt['is_negative']:
-                    bot.send_message(chat_id=TELEGRAM_ID, text=f"У вас проверили работу {attempt['lesson_title']} \n"
-                                                               f"https://dvmn.org{attempt['lesson_url']}\n"
-                                                               f"В работе нашлись ошибки")
-                else:
-                    bot.send_message(chat_id=TELEGRAM_ID, text=f"У вас проверили работу {attempt['lesson_title']} \n"
-                                                               f"https://dvmn.org{attempt['lesson_url']}\n"
-                                                               f"В работе нет ошибок")
-        params = {
-            'timestamp': json_data['last_attempt_timestamp']
-        }
-    except requests.exceptions.ReadTimeout:
-        time.sleep(0.1)
-    except requests.exceptions.ConnectionError:
-        time.sleep(1)
-    except ConnectionResetError:
-        time.sleep(1)
-    except requests.exceptions.HTTPError as exception:
-        print(exception.response.text)
-        time.sleep(1)
+if __name__ == '__main__':
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+
+    class MyLogsHandler(logging.Handler):
+
+        def emit(self, record):
+            log_entry = self.format(record)
+            bot.send_message(chat_id=TELEGRAM_ID, text=log_entry)
+
+
+    logger = logging.getLogger("MyLogsHandler")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler())
+    logger.info("Бот запущен!")
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {
+        'Authorization': 'Token ' + DEVMAN_TOKEN,
+    }
+    params = {}
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            json_data = response.json()
+            if json_data['status'] == 'found':
+                attempts = json_data['new_attempts']
+                for attempt in attempts:
+                    if attempt['is_negative']:
+                        bot.send_message(chat_id=TELEGRAM_ID,
+                                         text=f"У вас проверили работу {attempt['lesson_title']} \n"
+                                              f"https://dvmn.org{attempt['lesson_url']}\n"
+                                              f"В работе нашлись ошибки")
+                    else:
+                        bot.send_message(chat_id=TELEGRAM_ID,
+                                         text=f"У вас проверили работу {attempt['lesson_title']} \n"
+                                              f"https://dvmn.org{attempt['lesson_url']}\n"
+                                              f"В работе нет ошибок")
+            params = {
+                'timestamp': json_data['last_attempt_timestamp']
+            }
+        except requests.exceptions.ReadTimeout as exception:
+            logger.info(exception.response.text)
+            time.sleep(0.1)
+        except requests.exceptions.ConnectionError as exception:
+            logger.info(exception.response.text)
+            time.sleep(1)
+        except ConnectionResetError as exception:
+            logger.info(exception.response.text)
+            time.sleep(1)
+        except requests.exceptions.HTTPError as exception:
+            logger.info(exception.response.text)
+            time.sleep(1)
+
+# load_dotenv()
